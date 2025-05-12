b2ade9103d199017e37ed424301824946bc5d350
WenhuiYi-129 <1278226993@qq.com>
2025-05-12 15:31:21 +0800

    Add files via upload

--- fig2/SE_test.m
+++ fig2/SE_test.m
@@ -97,8 +97,6 @@ end
 % Initialization of Z
 Z_S = (2*rand(M, 3)-1)*A;
 Z_R = (2*rand(N,3,K)-1)*A;
-Z_alpha = zeros(K+1,1);
-Z_beta = zeros(K+1,1);
 % H
 H_channel = zeros(M,N,K);
 W_precoding = zeros(M,N,K);
@@ -139,20 +137,20 @@ cap_before = cap_temp;
 % update {z_n}, fix Q and {r_n}
 [Z_S] = update_z(Z_S, R_s, d);
 % update {r_r}, fix {r_s}, alpha, beta and {z_n}
-for u=1:K
-R_rr = R_r(:,:,u);
-Z_Rr = Z_R(:,:,u);
-R_rr = update_rr(p_max, noise,location_u,Mh,Mv,Nh,Nv,K,L,Z_Rr, R_s, R_rr, u, R_r, theta_r, phi_r, theta_t,phi_t,alpha,beta,path_loss, lambda, A);
-R_r(:,:,u) = R_rr;
-[Z_Rr ] = update_z(Z_Rr , R_rr , d);
-Z_R(:,:,u) = Z_Rr;
-end
+% for u=1:K
+% R_rr = R_r(:,:,u);
+% Z_Rr = Z_R(:,:,u);
+% R_rr = update_rr(p_max, noise,location_u,Mh,Mv,Nh,Nv,K,L,Z_Rr, R_s, R_rr, u, R_r, theta_r, phi_r, theta_t,phi_t,alpha,beta,path_loss, lambda, A);
+% R_r(:,:,u) = R_rr;
+% [Z_Rr ] = update_z(Z_Rr , R_rr , d);
+% Z_R(:,:,u) = Z_Rr;
+% end
 
-[alpha] = update_alpha(p_max, noise,location_u,Mh,Mv,Nh,Nv,K,L,Z_alpha, R_s, R_r, theta_r, phi_r, theta_t,phi_t,alpha,beta,path_loss, lambda, A);
-[Z_alpha ] = update_z(Z_alpha , alpha , d);
+[alpha] = update_alpha(noise,location_u,Mh,Mv,Nh,Nv,K,L,R_s, R_r, theta_r, phi_r, theta_t,phi_t,alpha,beta,path_loss, lambda, A);
 
-[beta] = update_beta(p_max, noise,location_u,Mh,Mv,Nh,Nv,K,L,Z_alpha, R_s, R_r, theta_r, phi_r, theta_t,phi_t,alpha,beta,path_loss, lambda, A);
-[Z_beta ] = update_z(Z_alpha , beta , d);
+
+[beta] = update_beta(noise,location_u,Mh,Mv,Nh,Nv,K,L, R_s, R_r, theta_r, phi_r, theta_t,phi_t,alpha,beta,path_loss, lambda, A);
+
 
 H_channel = zeros(M,N,K);
 W_precoding = zeros(M,N,K);
